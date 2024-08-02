9df89119ef1ea59e58b2078d0c298009c497439f
amineziraoui <amine@bearstudio.fr>
2024-08-02 20:13:54 +0100

    Fix drawer not openning

--- app/src/components/drawer.js
+++ app/src/components/drawer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { ActivitiesIcon, HomeIcon, PeopleIcon, ProjectsIcon } from "../assets/Icons";
 
@@ -7,14 +7,16 @@ const Drawer = () => {
 };
 
 const DrawerDesktop = () => {
+
+    const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="flex-shrink-0 overflow-y-auto">
       <ul className={`list-none px-3 z-10 border-r border-[#a0a6b124] space-y-2 translate-x-0 duration-200`}>
-        <Section title="Général">
-          <Link to="/" title="Home" Icon={HomeIcon} />
-          <Link to="/project" title="Projects" Icon={ProjectsIcon} />
-          <Link to="/activity" title="Activities" Icon={ActivitiesIcon} />
-          <Link to="/user" title="People" Icon={PeopleIcon} />
+        <Section title="Général" setIsOpen={setIsOpen}>
+          <Link to="/" title="Home" Icon={HomeIcon} isOpen={isOpen} />
+          <Link to="/project" title="Projects" Icon={ProjectsIcon}  isOpen={isOpen}/>
+          <Link to="/activity" title="Activities" Icon={ActivitiesIcon}  isOpen={isOpen}/>
+          <Link to="/user" title="People" Icon={PeopleIcon}  isOpen={isOpen}/>
         </Section>
 
         <div className="h-10" />
@@ -23,7 +25,7 @@ const DrawerDesktop = () => {
   );
 };
 
-const Link = ({ Icon, title, to, onClick = () => {} }) => {
+const Link = ({ Icon, title, to, isOpen, onClick = () => {} }) => {
   return (
     <li>
       <NavLink
@@ -34,22 +36,22 @@ const Link = ({ Icon, title, to, onClick = () => {} }) => {
         to={to}
         activeClassName="bg-[#0560FD] !text-[#FFFFFF] !fill-[#FFFFFF]">
         <Icon />
-        <span>{title}</span>
+        {isOpen && <span>{title}</span>}
       </NavLink>
     </li>
   );
 };
 
-const Section = ({ children, title }) => {
+const Section = ({ children, title, isOpen, setIsOpen }) => {
   return (
     <div>
       <h1
         className="flex gap-1 items-center uppercase text-[10px] text-gray-400 tracking-wide font-semibold mt-4 cursor-pointer hover:underline mb-2"
-        onClick={() => setOpen((o) => !o)}>
+        onClick={() => setIsOpen((o) => !o)}>
         {title}
       </h1>
 
-      {children}
+      {children} 
     </div>
   );
 };
